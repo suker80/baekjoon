b6fe8165a331aec6f0e0794a67398af13aed1f08
suker <suker800@gmail.com>
2022-04-21 12:56:34 +0900

    2661 좋은 수열.py

---
+++ G4/2661 좋은 수열.py
@@ -0,0 +1,34 @@
+n = int(input())
+
+answer = float('inf')
+import sys
+
+
+def isGood(String):
+    for i in range(1, len(String) // 2 + 1):
+        for j in range(len(String) - i):
+            left = String[j:j + i]
+            right = String[j + i:j + i + i]
+            if left == right:
+                return False
+
+    return True
+
+
+def solve(current):
+    global answer
+    if not isGood(current):
+        return
+    elif len(current) == n:
+        print(current)
+        sys.exit()
+        return
+
+    for i in ['1', '2', '3']:
+        if current and current[-1] == i:
+            continue
+        solve(current + i)
+
+
+solve('')
+print(answer)
