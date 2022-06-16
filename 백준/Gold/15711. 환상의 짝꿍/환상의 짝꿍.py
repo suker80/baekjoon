611f6c56d254fa5d1ad4ec7457f85ce5a0a1d0f4
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2022-06-16 23:30:33 +0900

    [Gold III] Title: 환상의 짝꿍, Time: 1884 ms, Memory: 141144 KB -BaekjoonHub

---
+++ 백준/Gold/15711. 환상의 짝꿍/환상의 짝꿍.py
@@ -0,0 +1,33 @@
+t = int(input())
+
+max_thieve = 2_000_001
+thieves = [False] * max_thieve
+
+for i in range(2, max_thieve):
+    for j in range(i + i, max_thieve, i):
+        thieves[j] = True
+thieves[1] = True
+primes = [i for i in range(2, max_thieve) if not thieves[i]]
+
+for i in range(t):
+    a, b = map(int, input().split())
+    s = a + b
+    if s % 2 == 0:
+        if s > 2:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        s -= 2
+        if s > max_thieve:
+            for p in primes:
+                if s % p == 0:
+                    print("NO")
+                    break
+            else:
+                print("YES")
+        else:
+            if thieves[s]:
+                print("NO")
+            else:
+                print("YES")
