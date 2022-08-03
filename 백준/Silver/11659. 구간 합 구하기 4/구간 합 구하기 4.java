d229d1af0252df4ed4d1b557552c5495b949a4fc
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2022-08-03 09:07:36 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1764 ms, Memory: 70160 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n, m;
+    static int start, end;
+
+    public static void main(String[] args) throws IOException {
+        String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
+
+        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + array[i - 1];
+        }
+        for (int i = 0; i < m; i++) {
+            String[] query = br.readLine().split(" ");
+            start = Integer.parseInt(query[0]);
+            end = Integer.parseInt(query[1]);
+            System.out.println(prefix[end] - prefix[start - 1]);
+
+        }
+
+
+    }
+}
