29e879545d936c9404bf6bb267acd5e55019c81c
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2022-10-04 14:08:41 +0900

    [Gold IV] Title: 스도쿠, Time: 1064 ms, Memory: 311604 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -11,12 +11,24 @@ public class Main {
     private static int[][] board = new int[9][9];
     private static int zeroCnt = 0;
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static HashSet<Integer>[] VerticalSet, HorizontalSet, areaSet;
 
     public static void main(String[] args) throws IOException {
+        VerticalSet = new HashSet[9];
+        HorizontalSet = new HashSet[9];
+        areaSet = new HashSet[9];
+        for (int i = 0; i < 9; i++) {
+            VerticalSet[i] = new HashSet<>();
+            HorizontalSet[i] = new HashSet<>();
+            areaSet[i] = new HashSet<>();
+        }
         for (int i = 0; i < 9; i++) {
             String line = br.readLine();
             for (int j = 0; j < 9; j++) {
                 board[i][j] = line.charAt(j) - '0';
+                VerticalSet[i].add(board[i][j]);
+                HorizontalSet[j].add(board[i][j]);
+                areaSet[(i / 3) * 3 + j / 3].add(board[i][j]);
                 if (board[i][j] == 0) {
                     zeroCnt += 1;
                 }
@@ -44,22 +56,19 @@ public class Main {
         for (int idx = index; idx < 81; idx++) {
             int i = idx / 9;
             int j = idx % 9;
-
-
             if (board[i][j] == 0) {
-                int finalJ = j;
-                Set<Integer> HorizontalSet = Arrays.stream(board[i]).boxed().collect(toSet());
-                Set<Integer> VerticalSet = Arrays.stream(board).map(ints -> ints[finalJ]).collect(toSet());
-                Set<Integer> areaSet = new HashSet<>();
-                for (int k = (i / 3) * 3; k < (i / 3 + 1) * 3; k++) {
-                    for (int l = (j / 3) * 3; l < (j / 3 + 1) * 3; l++) {
-                        areaSet.add(board[k][l]);
-                    }
-                }
+                int areaIdx = (i / 3) * 3 + j / 3;
                 for (int n = 1; n <= 9; n++) {
-                    if (!HorizontalSet.contains(n) && !VerticalSet.contains(n) && !areaSet.contains(n)) {
+                    if (!HorizontalSet[j].contains(n) && !VerticalSet[i].contains(n) && !areaSet[areaIdx].contains(n)) {
+
                         board[i][j] = n;
+                        HorizontalSet[j].add(n);
+                        VerticalSet[i].add(n);
+                        areaSet[areaIdx].add(n);
                         solve(i * 9 + j, depth + 1);
+                        HorizontalSet[j].remove(n);
+                        VerticalSet[i].remove(n);
+                        areaSet[areaIdx].remove(n);
                         board[i][j] = 0;
                     }
                 }
