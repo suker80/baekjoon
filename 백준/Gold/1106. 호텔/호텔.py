9e9ca52a4bbf303f74015591b7ff1c68fdfe3092
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2023-03-02 08:18:55 +0900

    [Gold V] Title: 호텔, Time: 196 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+cities = [list(map(int, input().split())) for _ in range(m)]
+dp = [float('inf')] * 1234
+
+for money, customer in cities:
+    dp[customer] = min(dp[customer], money)
+
+for i in range(len(dp)):
+    for j in range(m):
+        money, customer = cities[j]
+
+        if i - customer >= 0:
+            dp[i] = min(dp[i - customer] + money, dp[i])
+
+answer = float('inf')
+for i in range(n, len(dp)):
+    answer = min(answer, dp[i])
+print(answer)
