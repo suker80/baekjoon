ee7b2a5ee220f8d886aeffd9cfcc8419eadc7f26
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2023-04-17 23:56:13 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 532 ms, Memory: 127808 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -2,16 +2,16 @@ import sys
 from heapq import heappush, heappop
 input = sys.stdin.readline
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
+arr = [tuple(map(int, input().split())) for _ in range(n)]
 
 arr.sort()
 heap = []
-heappush(heap,[arr[0][1],arr[0][0]])
+heappush(heap,arr[0][1])
 answer = 0
 for i in range(1, n):
     start, end = arr[i]
-    heappush(heap, [end,start])
-    while heap and heap[0][0] <= start:
+    heappush(heap, end)
+    while heap and heap[0] <= start:
         heappop(heap)
     answer = max(len(heap), answer)
 print(answer)
