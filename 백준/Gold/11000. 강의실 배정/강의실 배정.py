83d3009ba7674ff90d6a697100acf39434bba1d8
Choi Tae gyu <39821474+suker80@users.noreply.github.com>
2023-10-31 14:49:16 +0900

    [Gold V] Title: 강의실 배정, Time: 512 ms, Memory: 138176 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,17 +1,15 @@
-import sys
-import heapq
-
-input = sys.stdin.readline
 n = int(input())
-lessons = [list(map(int, input().split())) for _ in range(n)]
-
-lessons.sort()
-
+from heapq import *
 heap = []
-for lesson in lessons:
-    if heap and heap[0] <= lesson[0]:
-        heapq.heappop(heap)
-    heapq.heappush(heap, lesson[1])
 
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-print(len(heap))
+arr.sort()
+answer = 0
+for i in range(n):
+    if heap and heap[0] <= arr[i][0]:
+        heappop(heap)
+    heappush(heap,arr[i][1])
+    answer = max(answer,len(heap))
+
+print(answer)
